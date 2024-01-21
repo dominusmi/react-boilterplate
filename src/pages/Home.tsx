746e2c70ae86e381a16ea9fd2d858823927a5ca2
dominusmi <barp.edoardo@gmail.com>
2024-01-21 12:59:37 +0100

    Add example translation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,11 @@ import { useTranslation } from "react-i18next";
 
 
 const Home: React.FC = () => {
-    const t = useTranslation();
+    const {t} = useTranslation();
     return (
-        <div></div>
+        <div>
+            <h1>{t("example-h1")}</h1>
+        </div>
     )
 }
 
